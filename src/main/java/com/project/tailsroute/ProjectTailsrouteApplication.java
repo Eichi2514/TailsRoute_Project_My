d75804d12be569e33291f81f86f02379ae3c1782
Eichi2514 <picon1317@gmail.com>
2024-10-20 21:27:22 +0900

    문자 알림 서비스 추가

--- src/main/java/com/project/tailsroute/ProjectTailsrouteApplication.java
+++ src/main/java/com/project/tailsroute/ProjectTailsrouteApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjectTailsrouteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjectTailsrouteApplication.class, args);
-	}
+    // 메인 메소드, 스프링 애플리케이션 실행
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectTailsrouteApplication.class, args); // 스프링 부트 애플리케이션 시작
+
+    }
 }
